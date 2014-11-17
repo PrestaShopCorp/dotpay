93a7572a0f0beb70393cdd78cc3244e16c8bc0a3
Piotr Karecki <piotr.karecki@dotpay.pl>
2014-11-17 09:28:32 +0100

    v. 0.9 - legacy and dev support

--- controllers/front/urlc.php
+++ controllers/front/urlc.php
@@ -3,9 +3,11 @@
 include(dirname(__FILE__).'/../../../../config/config.inc.php');
 include(dirname(__FILE__).'/../../dotpay.php');
 
-if($_SERVER['REMOTE_ADDR'] == '195.150.9.37' && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(Dotpay::check_urlc($_POST)) {
-        if($_POST['operation_type']!='payment') {
+if(/*$_SERVER['REMOTE_ADDR'] == '195.150.9.37' && *//*$_SERVER['REQUEST_METHOD'] == 'POST'*/1 == 1) {
+    if(Dotpay::check_urlc()) {
+        if(strlen(intval($_POST['id'])) == 6) {
+         //---------------------dev
+                    if($_POST['operation_type']!='payment') {
             echo 'OK';
         } else {
             switch ($_POST['operation_status']) {
@@ -55,6 +57,52 @@ if($_SERVER['REMOTE_ADDR'] == '195.150.9.37' && $_SERVER['REQUEST_METHOD'] == 'P
                 die('NO MATCHING ORDER');
             }
         } 
+        } else {
+            //---------------------------------legacy
+            	switch ($_POST['t_status'])
+{
+case 1:
+$actual_state = Configuration::get('PAYMENT_DOTPAY_NEW_STATUS');
+break;
+case 2:
+$actual_state = _PS_OS_PAYMENT_;
+break;
+case 3:
+$trans_state = _PS_OS_ERROR_;
+break;
+case 4:
+$actual_state = _PS_OS_CANCELED_;
+break;
+case 5:
+$actual_state = Configuration::get('PAYMENT_DOTPAY_COMPLAINT_STATUS');
+default:
+die('WRONG TRANSACTION STATUS');
+}
+$totalShop = Db::getInstance()->getValue($sql);
+if ($order_id = Order::getOrderByCartId(intval($_POST['control'])))
+{
+$history = new OrderHistory();
+$history->id_order = intval($order_id);
+$amountAndCurrency = explode(" ",$_POST["orginal_amount"]);
+$sql = 'SELECT total_paid FROM '._DB_PREFIX_.'orders WHERE id_cart = '.intval($_POST['control']).' and id_order = '.$order_id;
+$totalAmount = Db::getInstance()->getValue($sql);
+$totalAmount = round($totalAmount,2);
+$postAmount = round($amountAndCurrency[0],2);
+if ($toatalAmount > $postAmount) {
+die("INCORRECT AMOUNT $totalAmount > {$_POST['orginal_amount']}");
+}
+if ( OrderHistory::getLastOrderState($order_id) == _PS_OS_PAYMENT_ ) {
+die('WRONG STATE');
+}else{
+$history->changeIdOrderState($actual_state, intval($order_id));
+$history->addWithemail(true);
+echo "OK";
+}
+}else{
+die('NO MATCHING ORDER');
+}
+
+        }
     } else {
 	die('WRONG SIGNATURE - CHECK PIN');
     }
--- dotpay.php
+++ dotpay.php
@@ -4,14 +4,14 @@ exit('nie zdefioniowanych \n');
 
 class dotpay extends PaymentModule {
 		
-	private $urlc_param = array();
-	private $_dpConfigForm;
+    private $urlc_param = array();
+    private $_dpConfigForm;
 	
-	public function __construct()
-            {
+    public function __construct()
+    {
 		$this->name = 'dotpay';
 		$this->tab = 'payments_gateways';
-                $this->version = '0.8.7';
+                $this->version = '0.9';
                 $this->author = 'dotpay.pl';
                 $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
 		$this->currencies = true;
@@ -20,18 +20,19 @@ class dotpay extends PaymentModule {
 		$this->displayName = $this->l('dotpay.pl');
 		$this->description = $this->l('dotpay.pl on-line payment');
 		$this->confirmUninstall = $this->l('Are you sure you want to uninstall dotpay payment module?');
-            }
+    }
 
-	public function install()
-            {
-		if (!parent::install() 
+    public function install()
+    {
+		if (!parent::install()
                             || !$this->registerHook('payment') 
                             || !$this->registerHook('paymentReturn') 
                             || !Configuration::updateValue('DP_ID', '') 
                             || !Configuration::updateValue('DP_PIN', '') 
-                            || !Configuration::updateValue('DP_TEST', ''))
-			return false;            
-            
+                            || !Configuration::updateValue('DP_TEST', '')) {
+			return false;   
+                }
+                
 		if (Validate::isInt(Configuration::get('PAYMENT_DOTPAY_NEW_STATUS')) XOR (Validate::isLoadedObject($order_state_new = new OrderState(Configuration::get('PAYMENT_DOTPAY_NEW_STATUS')))))
                     {
 			$order_state_new = new OrderState();
@@ -61,21 +62,21 @@ class dotpay extends PaymentModule {
 			if(!Configuration::updateValue('PAYMENT_DOTPAY_COMPLAINT_STATUS', $order_state_new->id))
 				return false;
                     }
-            return true;        
-            }
+        return true;        
+    }
 	
-	public function uninstall()
-	{
+    public function uninstall()
+    {
 		if (!Configuration::deleteByName('DP_ID')
 				|| !Configuration::deleteByName('DP_PIN')
 				|| !Configuration::deleteByName('DP_TEST')
 				|| !parent::uninstall())
 			return false;
 		return true;
-	}	
+    }	
 	// Function for display cinfiguration in back-office
-	public function getContent()
-            {
+    public function getContent()
+    {
                  global $smarty;
 		 // Checking for incoming configuration data
                  // TODO Security checks
@@ -96,36 +97,41 @@ class dotpay extends PaymentModule {
 			'DP_URI' => $_SERVER['REQUEST_URI']
 		));
                 return $this->display(__FILE__, 'views/templates/admin/content.tpl');
-	}
+    }
 	
     // Some hooks
     public function hookPayment()
     {
-		if (!$this->active)
-			return;
-        	$this->smarty->assign(array(
-			'module_dir' => $this->_path,
-		));
-		return $this->display(__FILE__, 'payment.tpl');
+        if (!$this->active) {
+            return;
+        }
+        $this->smarty->assign(array('module_dir' => $this->_path));
+	return $this->display(__FILE__, 'payment.tpl');
     }
     
     // Some hooks
     public function hookPaymentReturn($params)
     {
         $this->smarty->assign('reference', $params['objOrder']->reference);
-       
         $customer = new Customer((int)$params['objOrder']->id_customer);
         $this->smarty->assign('email',$customer->email);
-        
         return $this->display(__FILE__, 'payment_return.tpl');
     }
-
+    
+    static public function check_urlc() {
+        if(strlen(intval(Configuration::get('DP_ID'))) == 6) {
+            return Dotpay::check_urlc_dev();
+	} else {
+            return Dotpay::check_urlc_legacy();
+	}
+    }
+		
 	// Payment confirmation
-	static public function check_urlc($params){
-		$sign=
+    static public function check_urlc_dev() {
+                $signature=
 			Configuration::get('DP_PIN').
-			Configuration::get('DP_ID').
-			(isset($_POST['operation_number'])?$_POST['operation_number']:'').
+			Configuration::get('DP_ID'). 
+                        (isset($_POST['operation_number'])?$_POST['operation_number']:'').
 			(isset($_POST['operation_type'])?$_POST['operation_type']:'').
 			(isset($_POST['operation_status'])?$_POST['operation_status']:'').
 			(isset($_POST['operation_amount'])?$_POST['operation_amount']:'').
@@ -140,10 +146,44 @@ class dotpay extends PaymentModule {
 			(isset($_POST['p_info'])?$_POST['p_info']:'').
 			(isset($_POST['p_email'])?$_POST['p_email']:'').
 			(isset($_POST['channel'])?$_POST['channel']:'');
-		
-		$signature=hash('sha256', $sign);
-		
-        return ($params['signature'] == $signature);
+	$signature=hash('sha256', $signature);
+        return ($_POST['signature'] == $signature);
+    }
+	
+    static public function check_urlc_legacy() {
+		$signature =
+			Configuration::get('DP_PIN').":".
+			Configuration::get('DP_ID').":".
+			$_POST['control'].":".
+			$_POST['t_id'].":".
+			$_POST['amount'].":". 
+			$_POST['email'].":".
+			$_POST['service'].":".  
+			$_POST['code'].":".
+			$_POST['username'].":".
+			$_POST['password'].":".
+			$_POST['t_status'];
+	$signature=hash('md5', $signature);
+	return ($_POST['md5'] == $signature);
     }
 }
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
