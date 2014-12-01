bee5e8042d78123d4199ee1e8e4c37fbadc57a87
Piotr Karecki <piotr.karecki@dotpay.pl>
2014-12-01 13:55:24 +0100

    Version 1.0.1 - forgotten increment
    Fix: $order_id empty value - "NO MATCHING ORDER" error.
    FIx: PHP Fatal error:  Call to a member function getPageLink() on a non-object in classes/Mail.php on line 251

--- dotpay.php
+++ dotpay.php
@@ -11,7 +11,7 @@ class dotpay extends PaymentModule {
     {
 		$this->name = 'dotpay';
 		$this->tab = 'payments_gateways';
-                $this->version = '1.0.0';
+                $this->version = '1.0.1';
                 $this->author = 'dotpay.pl';
                 $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
 		$this->currencies = true;
