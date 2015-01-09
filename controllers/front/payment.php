c2ce83a43a2404ece3592b1af7781042f0a4796d
Piotr Karecki <piotr.karecki@dotpay.pl>
2015-01-09 17:10:58 +0100

    v 1.1.4 dotpay payment module -some validation and fixes

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -5,36 +5,55 @@ class DotpayPaymentModuleFrontController extends ModuleFrontController
         
     public function initContent()
     {
+        if (!Tools::getValue("ajax"))
+        {
                 $this->display_column_left = false;	
-                
+                parent::initContent();
+                                
                 if (Tools::getIsset('control'))
                 {
                     $cart = new Cart((int)Tools::getValue('control'));
                 } else {
                     $cart = $this->context->cart;              
                 }
-                //TODO Validation
+
+                if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
+                        Tools::redirect('index.php?controller=order&step=1');
+                
+                // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
+                $authorized = false;
+                foreach (Module::getPaymentModules() as $module)
+                if ($module['name'] == 'dotpay')
+                {
+                    $authorized = true;
+                    break;
+                }
+                if (!$authorized)
+                    die('This payment method is not available.');                 
+
                 $address = new Address($cart->id_address_invoice);
                 $customer = new Customer($cart->id_customer);
+                if (!Validate::isLoadedObject($customer))
+                    return;
                 $currency_info = Currency::getCurrency($cart->id_currency);
-                if ($cart->OrderExists() == false) 
+                if ($cart->OrderExists() == true) 
                 {
-                        $this->context->smarty->assign(array(
-                                'module_dir' => $this->module->getPathUri(),
-                                'dp_test' => Configuration::get('DP_TEST'),
-                                'dp_id' => Configuration::get('DP_ID'),
-                                'dp_control' => (int)$cart->id,
-                                'dp_amount' => $cart->getOrderTotal(),
-                                'dp_desc' => Configuration::get('PS_SHOP_NAME'), 
-                                'customer' => $customer,
-                                'address' => $address,
-                                'currency' => $currency_info["iso_code"]
-                        ));
-                        $this->setTemplate('payment.tpl');
-                } elseif (!Tools::getValue("ajax")) {
-                            Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.Order::getOrderByCartId($cart->id).'&key='.$customer->secure_key);
-                       }      
-        }
+                        Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.Order::getOrderByCartId($cart->id).'&key='.$customer->secure_key);                    
+                        exit;
+                }
+                $this->context->smarty->assign(array(
+                        'module_dir' => $this->module->getPathUri(),
+                        'dp_test' => Configuration::get('DP_TEST'),
+                        'dp_id' => Configuration::get('DP_ID'),
+                        'dp_control' => (int)$cart->id,
+                        'dp_amount' => $cart->getOrderTotal(),
+                        'dp_desc' => Configuration::get('PS_SHOP_NAME'), 
+                        'customer' => $customer,
+                        'address' => $address,
+                        'currency' => $currency_info["iso_code"]
+                ));
+                $this->setTemplate('payment.tpl');
+    }}
         
 	public function displayAjax()
         {
