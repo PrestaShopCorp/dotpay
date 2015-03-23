cf46d91df8c050ae945523c4bee154c6dfacf2bc
Piotr Karecki <piotr.karecki@dotpay.pl>
2015-03-23 04:43:32 +0100

    v 1.4.2 dotpay payment module -improved callback controler -fixed check on payment controller -moved return template

--- controllers/front/callback.php
+++ controllers/front/callback.php
@@ -26,67 +26,61 @@
 
 class dotpaycallbackModuleFrontController extends ModuleFrontController
 {
-	public function displayAjax()
+    public function displayAjax()
+    {
+        if($_SERVER['REMOTE_ADDR'] <> '195.150.9.37' or $_SERVER['REQUEST_METHOD'] <> 'POST') 
+            die("PrestaShop - ERROR");
+
+        if(!Dotpay::check_urlc_legacy())
+            die("PrestaShop - LEGACY MD5 ERROR - CHECK PIN");
+
+        $cart = new Cart((int)Tools::getValue('control'));
+        $customer = new Customer((int)$cart->id_customer);
+        $currency = Currency::getCurrency((int)$cart->id_currency);
+        $total = (float)$cart->getOrderTotal();
+        $price = number_format($total, 2);
+        $price .= " ".$currency["iso_code"];
+        $orginal_amount = trim(Tools::getValue('orginal_amount'));
+
+        if($price <> $orginal_amount)
+            die('PrestaShop - NO MATCH OR WRONG AMOUNT - '.$price.' <> '.$orginal_amount);
+
+        switch (Tools::getValue('t_status'))
         {
-                if($_SERVER['REMOTE_ADDR'] == '195.150.9.37' && $_SERVER['REQUEST_METHOD'] == 'POST') 
-                {
-                        if(Dotpay::check_urlc_legacy())
-                        {
-                                switch (Tools::getValue('t_status'))
-                                {
-                                    case 1:
-                                        $actual_state = Configuration::get('PAYMENT_DOTPAY_NEW_STATUS');
-                                        break;
-                                    case 2:
-                                        $actual_state = _PS_OS_PAYMENT_;
-                                        break;
-                                    case 3:
-                                        $actual_state = _PS_OS_ERROR_;
-                                        break;
-                                    case 4:
-                                        $actual_state = _PS_OS_ERROR_;
-                                        break;
-                                    case 5:
-                                        $actual_state = Configuration::get('PAYMENT_DOTPAY_COMPLAINT_STATUS');   
-                                    default:
-                                        die ("PrestaShop - WRONG TRANSACTION STATUS");
-                                }
-                                $cart = new Cart((int)Tools::getValue('control'));
-                                //$address = new Address($cart->id_address_invoice);
-                                $customer = new Customer((int)$cart->id_customer);
-                                $total = (float)($cart->getOrderTotal(true, Cart::BOTH));
-
-                                if ($cart->OrderExists() == false)
-                                    $this->module->validateOrder($cart->id, Configuration::get('PAYMENT_DOTPAY_NEW_STATUS'), (float)($cart->getOrderTotal(true, Cart::BOTH)), $this->module->displayName, NULL, array(), (int)$cart->id_currency, false, $customer->secure_key);
-                                   
-                                if ($order_id = Order::getOrderByCartId((int)Tools::getValue('control'))) 
-                                {
-                                        $sql = 'SELECT total_paid FROM '._DB_PREFIX_.'orders WHERE id_cart = '.$cart->id.' and id_order = '.$order_id;
-                                        $totalAmount = round(Db::getInstance()->getValue($sql),2);
-                                        $dotpay_amount = round(Tools::getValue('orginal_amount'),2);
-                                        if ($totalAmount <> $dotpay_amount) 
-                                                die('PrestaShop - INCORRECT AMOUNT '.$totalAmount.' <> '.$dotpay_amount);                                        
-                                      
-                                        if (strpos($totalAmount, ".") == false)
-                                                $totalAmount .= ".00";
-                                        $currency = Currency::getCurrency($cart->id_currency);
-                                        $totalAmount .= " ".$currency["iso_code"];
-                                        $orginal_amount = trim(Tools::getValue('orginal_amount'));
-                                        if ($totalAmount <> $orginal_amount) 
-                                                die('PrestaShop - INCORRECT ORG. AMOUNT '.$totalAmount.' <> '.$orginal_amount);
-
-                                        $history = new OrderHistory();
-                                        $history->id_order = $order_id;
-                                        if ( OrderHistory::getLastOrderState($order_id) == _PS_OS_PAYMENT_ ) 
-                                        {
-                                                die('PrestaShop - WRONG STATE');
-                                        } else {
-                                                $history->changeIdOrderState($actual_state, $order_id);
-                                                $history->addWithemail(true);
-                                                die ("OK");
-                                        }
-                                } else die('PrestaShop - NO MATCHING ORDER');
-                        } else die ("PrestaShop - LEGACY MD5 ERROR - CHECK PIN");
-                } else die("PrestaShop - ERROR");
+            case 1:
+                $actual_state = Configuration::get('PAYMENT_DOTPAY_NEW_STATUS');
+                break;
+            case 2:
+                $actual_state = _PS_OS_PAYMENT_;
+                break;
+            case 3:
+                $actual_state = _PS_OS_ERROR_;
+                break;
+            case 4:
+                $actual_state = _PS_OS_ERROR_;
+                break;
+            case 5:
+                $actual_state = Configuration::get('PAYMENT_DOTPAY_COMPLAINT_STATUS');   
+            default:
+                die ("PrestaShop - WRONG TRANSACTION STATUS");
         }
+        
+        if($cart->OrderExists() == false)
+        {
+            $this->module->validateOrder($cart->id, $actual_state, $total, $this->module->displayName, NULL, array(), (int)$cart->id_currency, false, $customer->secure_key);
+            echo("OK");
+        }
+        else
+        {
+            $order_id = Order::getOrderByCartId((int)Tools::getValue('control'));
+            $history = new OrderHistory();
+            $history->id_order = $order_id;
+            if(OrderHistory::getLastOrderState($order_id) <> $actual_state)
+            {
+                $history->changeIdOrderState($actual_state, $order_id);
+                $history->addWithemail(true);
+            }
+            echo("OK");
+        }
+    }
 }
--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -28,72 +28,67 @@ class dotpaypaymentModuleFrontController extends ModuleFrontController
 {   
     public function initContent()
     {
-                $this->display_column_left = false;	
-                parent::initContent();
-                $control=(int)Tools::getValue('control');
-                $cart = $this->context->cart;              
-                if (!empty($control))
-                    $cart = new Cart($control);
-
-                if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
-                        Tools::redirect('index.php?controller=order&step=1');
-                
-                // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
-                $authorized = false;
-                foreach (Module::getPaymentModules() as $module)
-                if ($module['name'] == 'dotpay')
-                {
-                    $authorized = true;
-                    break;
-                }
-                if (!$authorized)
-                    die('This payment method is not available.');                 
-
-                
-		$customer = new Customer($cart->id_customer);
-		if (!Validate::isLoadedObject($customer))
-			Tools::redirect('index.php?controller=order&step=1');
-                $address = new Address($cart->id_address_invoice);
-                $params = null;
-                $template = "payment_return";
-                if ($cart->OrderExists() == true) 
-                    Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.Order::getOrderByCartId($cart->id).'&key='.$customer->secure_key);                    
-                elseif (Tools::getValue("status") == "OK")
-                    $form_url= $this->context->link->getModuleLink('dotpay', 'payment', array('control' => $cart->id, 'status' => 'OK'));
-                else {
-                    $template = "payment";
-                    $form_url = "https://ssl.dotpay.pl/";
-                    $currency = Currency::getCurrency($cart->id_currency);
-                    if (Configuration::get('DP_TEST')==1) $form_url.="test_payment/";
-                    $params = array(
-                            'id' => Configuration::get('DP_ID'),
-                            'amount' => (float)$cart->getOrderTotal(true, Cart::BOTH),
-                            'currency' => $currency["iso_code"],
-                            'description' => Configuration::get('PS_SHOP_NAME'), 
-                            'url' => $this->context->link->getModuleLink('dotpay', 'payment', array('control' => $cart->id)),                        
-                            'type' => 0,                        
-                            'urlc' => $this->context->link->getModuleLink('dotpay', 'callback', array('ajax' => '1')),
-                            'control' => $cart->id,
-                            'firstname' => $customer->firstname,
-                            'lastname' => $customer->lastname,                        
-                            'email' => $customer->email,
-                            'street' => $address->address1,
-                            'city' => $address->city,
-                            'postcode'=> $address->postcode,
-                            'api_version' => 'legacy'
-                    );
-                    $chk = $params['id'].$params['amount'].$params['currency'].$params['description'].$params['control'].Configuration::get('DP_PIN');
-                    $chk = rawurlencode($chk);
-                    if(Configuration::get('DP_CHK'))
-                        $params['chk']=hash('md5', $chk);
-                }
-                $this->context->smarty->assign(array(
-                            'params' => $params,
-                            'module_dir' => $this->module->getPathUri(),
-                            'form_url' => $form_url,
-                            ));
-                $this->setTemplate($template.".tpl");
-                
- 
+        $this->display_column_left = false;	
+        parent::initContent();
+        $control=(int)Tools::getValue('control');
+        $cart = $this->context->cart;              
+        if (!empty($control))
+            $cart = new Cart($control);
+        if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
+                Tools::redirect('index.php?controller=order&step=1');
+        $customer = new Customer($cart->id_customer);
+        if (!Validate::isLoadedObject($customer))
+                Tools::redirect('index.php?controller=order&step=1');
+        $address = new Address($cart->id_address_invoice);
+        $params = null;
+        $template = "payment_return";
+        if ($cart->OrderExists() == true) 
+            Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.Order::getOrderByCartId($cart->id).'&key='.$customer->secure_key);                    
+        elseif (Tools::getValue("status") == "OK")
+            $form_url= $this->context->link->getModuleLink('dotpay', 'payment', array('control' => $cart->id, 'status' => 'OK'));
+        else 
+        {
+            // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
+            $authorized = false;
+            foreach (Module::getPaymentModules() as $module)
+            if ($module['name'] == 'dotpay')
+            {
+                $authorized = true;
+                break;
+            }
+            if (!$authorized)
+                die('This payment method is not available.');         
+            $template = "payment";
+            $form_url = "https://ssl.dotpay.pl/";
+            $currency = Currency::getCurrency($cart->id_currency);
+            if (Configuration::get('DP_TEST')==1) $form_url.="test_payment/";
+            $params = array(
+                    'id' => Configuration::get('DP_ID'),
+                    'amount' => (float)$cart->getOrderTotal(true, Cart::BOTH),
+                    'currency' => $currency["iso_code"],
+                    'description' => Configuration::get('PS_SHOP_NAME'), 
+                    'url' => $this->context->link->getModuleLink('dotpay', 'payment', array('control' => $cart->id)),                        
+                    'type' => 0,                        
+                    'urlc' => $this->context->link->getModuleLink('dotpay', 'callback', array('ajax' => '1')),
+                    'control' => $cart->id,
+                    'firstname' => $customer->firstname,
+                    'lastname' => $customer->lastname,                        
+                    'email' => $customer->email,
+                    'street' => $address->address1,
+                    'city' => $address->city,
+                    'postcode'=> $address->postcode,
+                    'api_version' => 'legacy'
+            );
+            $chk = $params['id'].$params['amount'].$params['currency'].$params['description'].$params['control'].Configuration::get('DP_PIN');
+            $chk = rawurlencode($chk);
+            if(Configuration::get('DP_CHK'))
+                $params['chk']=hash('md5', $chk);
+        }
+        $this->context->smarty->assign(array(
+            'params' => $params,
+            'module_dir' => $this->module->getPathUri(),
+            'form_url' => $form_url,
+            ));
+        $this->setTemplate($template.".tpl");
     }
 }
