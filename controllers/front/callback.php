e75a9e2021659711e6f8f8899c0906b5b30018d2
Piotr Karecki <piotr.karecki@dotpay.pl>
2015-04-08 09:16:48 +0200

    v1.4.4 dotpay payment module -Fixed translations -Fixed order state check on callback

--- controllers/front/callback.php
+++ controllers/front/callback.php
@@ -28,6 +28,10 @@ class dotpaycallbackModuleFrontController extends ModuleFrontController
 {
     public function displayAjax()
     {
+        
+	if($_SERVER['REMOTE_ADDR'] == '77.79.195.34')
+            die("PrestaShop - M.Ver: ".$this->module->version.", P.Ver: ". _PS_VERSION_ .", ID: ".Configuration::get('DP_ID').", Active: ".Configuration::get('DOTPAY_CONFIGURATION_OK').", Test: ".Configuration::get('DP_TEST').", CHK: ".Configuration::get('DP_CHK'));        
+        
         if($_SERVER['REMOTE_ADDR'] <> '195.150.9.37' or $_SERVER['REQUEST_METHOD'] <> 'POST') 
             die("PrestaShop - ERROR");
 
--- dotpay.php
+++ dotpay.php
@@ -37,7 +37,7 @@ class dotpay extends PaymentModule
 	{
 		$this->name = 'dotpay';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.4.3';
+		$this->version = '1.4.4';
                 $this->author = 'tech@dotpay.pl';
 
 		parent::__construct();
